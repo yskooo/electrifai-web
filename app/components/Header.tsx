6cde0f13db869561cf7ef82be4113a470c0ddf70
SiegfredLorelle <siegfredlorelle09@gmail.com>
2024-10-10 01:04:19 +0800

    style: swap pos of the mob nav btn & dark mode btn

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -80,7 +80,7 @@ export default function Header() {
       } shadow-lg flex items-center justify-between px-6 py-6 h-20`}
     >
       {/* Logo */}
-      <Link href="/" className="flex items-center">
+      <Link href="/" className="flex items-center mr-auto">
         <img src="/svgs/elec_logo.svg" alt="logo" className="w-20 h-20" />
         <span
           className={`text-xl font-bold ${
@@ -90,17 +90,6 @@ export default function Header() {
           Electrif<span className="italic">AI</span>
         </span>
       </Link>
-
-      {/* Mobile Menu Toggle */}
-      <button
-        className={`ml-auto lg:hidden ${
-          isDarkMode ? "text-white" : "text-[#0A0A0B]"
-        } focus:outline-none`}
-        onClick={toggleMenu}
-        aria-label="Toggle menu"
-      >
-        {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
-      </button>
 
       {/* Navigation Links */}
       <nav
@@ -128,11 +117,22 @@ export default function Header() {
       <button
         onClick={toggleDarkMode}
         aria-label="Toggle Dark Mode"
-        className={`ml-4 p-2 rounded-md transition-all duration-300 ease-in-out ${
+        className={`p-2 rounded-md transition-all duration-300 ease-in-out ${
           isDarkMode ? "bg-[#00000]" : "bg-white"
         }`}
       >
         {isDarkMode ? <Sun className="h-5 w-5" /> : <Moon className="h-5 w-5" />}
+      </button>
+
+      {/* Mobile Menu Toggle */}
+      <button
+        className={`ml-4 lg:hidden ${
+          isDarkMode ? "text-white" : "text-[#0A0A0B]"
+        } focus:outline-none`}
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+      >
+        {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
       </button>
     </header>
   );
