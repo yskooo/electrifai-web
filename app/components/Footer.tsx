ddc3f9806af5e752ed53234088cdc738545d5de3
SiegfredLorelle <siegfredlorelle09@gmail.com>
2024-10-13 09:26:40 +0800

    style: improve footer ui for sm screen

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,23 +9,23 @@ const Footer = () => {
 
   return (
     <footer
-      className={`flex flex-col sm:flex-row items-center justify-between py-8 px-8 sm:px-20 w-full text-white transition-all duration-500 ease-in-out ${
+      className={`flex flex-col lg:flex-row items-center justify-between gap-4 p-4 lg:p-8 w-full text-white transition-all duration-500 ease-in-out ${
         isDarkMode ? "bg-gradient-to-r from-[#000000] to-[#001027]" : "bg-gray-800"
       }`}
     >
-      <div className="mb-4 sm:mb-0 text-center sm:text-left">
+      <div className="mb-2 lg:mb-0 text-center lg:text-left">
         <p className="text-sm font-semibold">&copy; 2024 ElectricAI Sol&#39;n PH. All rights reserved.</p>
         <p className="text-xs text-white/60">Empowering energy solutions for a brighter tomorrow.</p>
       </div>
-      <div className="flex flex-col gap-3 text-center sm:text-left text-white/70">
 
-        <div className="flex items-center gap-2">
-          <Mail className="w-4 h-4" />
-          <span>contactelectrifai@gmail.com</span>
-        </div>
-        <div className="flex items-center gap-2">
-          <MapPin className="w-4 h-4" />
-          <span>Polytechnic University of the Philippines, Sta. Mesa, Philippines</span>
+      <div className="flex flex-col text-center gap-2 sm:text-left text-white/70">
+        <a href="mailto:contactelectrifai@gmail.com" className="flex justify-center lg:justify-start items-center">
+          <Mail className="mr-4 w-4 h-4 hidden lg:block" />
+          <span className="text-sm lg:text-base">contactelectrifai@gmail.com</span>
+        </a>
+        <div className="flex justify-center lg:justify-start items-center">
+          <MapPin className="mr-4 w-4 h-4 hidden lg:block" />
+          <span className="text-sm lg:text-base">Polytechnic University of the Philippines, Sta. Mesa, Philippines</span>
         </div>
       </div>
     </footer>
